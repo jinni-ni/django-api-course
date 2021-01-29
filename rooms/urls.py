79b5e69d5042a9a00c837dc6ef977c28425de8d3
김수진 <sjkim910115@gmail.com>
2021-01-30 05:54:06 +0900

    viewset

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
+from . import viewsets
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
 
 app_name = "rooms"
+router.register("", viewsets.RoomViewSet, basename="room" )
 
-urlpatterns = [
-    path("list/", views.ListRoomView.as_view()),
-    path("<int:pk>/", views.SeeRoomView.as_view()),
-]
+urlpatterns = router.urls
+
+# urlpatterns = [
+    # path("list/", views.ListRoomView.as_view()),
+    # path("<int:pk>/", views.SeeRoomView.as_view()),
+# ]
