d5a5958641c66b4823956b29af02180ffd59f168
김수진 <sjkim910115@gmail.com>
2021-01-29 15:44:22 +0900

    listview, retriveview

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
+from .models import Room
+from users.serializers import TinyUserSerializer
+# class RoomSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=140)
+#     price  = serializers.IntegerField()
+#     bedrooms = serializers.IntegerField()
+#     instant_book = serializers.BooleanField()
 
-class RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    price  = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
+class RoomSerializer(serializers.ModelSerializer):
+    user = TinyUserSerializer()
+    class Meta:
+        model = Room
+        fields = ("pk", "name", "price", "bedrooms", "instant_book", "user")
+
+class BigRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,13 @@
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, BigRoomSerializer
 
-# Create your views here.
+# # Create your views here.
+
+
 @api_view(["GET", "DELETE"])
 def list_rooms(request):
     rooms = Room.objects.all()
@@ -11,4 +15,16 @@ def list_rooms(request):
     return Response(data=serialized_rooms.data)
 
 
-    
+# class ListRoomView(APIView):
+#     def get(self, request):
+#         rooms = Room.objects.all()
+#         serializer = RoomSerializer(rooms, many=True)
+#         return Response(serializer.data)
+
+class ListRoomView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+class SeeRoomView(RetrieveAPIView):
+    queryset = Room.objects.all()
+    serializer_class = BigRoomSerializer
